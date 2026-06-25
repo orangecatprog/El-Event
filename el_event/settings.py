4a8eafef8d6d0317e15b874e4b48c7e5559fcd69
orangecatprog <luisrguezgam@gmail.com>
2026-06-25 10:06:36 +0200

    Update settings

--- el_event/settings.py
+++ el_event/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'local-key-only-for-developing')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
